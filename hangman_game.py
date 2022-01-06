f8e4e1c86a69338056f7955f82966326c78939f3
alteregoxiv <prasad.biswasxiv@gmail.com>
2022-01-06 20:26:15 +0530

    .gitignore , app.py , hangman_game.py , index.html added.... hangman_game.py is terminal representation of the hangman game

---
+++ hangman_game.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import time
+
+httpresponse = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+textdata = httpresponse.text
+jsondata = json.loads(textdata)
+
+mdata = jsondata[0]
+ndata = [i for i in mdata]
+l = len(mdata)
+s = ["_" for i in range(l)]
+
+life = 5
+while life>0:
+    if ("").join(s)==mdata:
+        time.sleep(1.5)
+        print("---------------------------------")
+        print("Yay! You Won")
+        print("The Word was : " + mdata)
+        print("---------------------------------")
+        break
+    
+    print()
+    print("[" + (" ").join(s) + "]")
+    print("Enter your letter : " , end = "")
+    q = input()
+    print()
+    time.sleep(1.5)
+
+    while len(q)>1 or q.isnumeric():
+        print("Please enter one letter : " , end = "")
+        q = input()
+        print()
+        time.sleep(1.5)
+
+    if q in ndata:
+        count = mdata.count(q)
+        for i in range(count):
+            w = ndata.index(q)
+            ndata[w] = 0
+            s[w] = q
+    else:
+        if q in s:
+            print("You've already guessed " + q)
+            continue
+        print()
+        life -= 1
+        if life!=0:
+            print("Wrong choice!")
+            print("LIFE LEFT : " + str(life))
+            print("Please try again")
+        else:
+            print("LIFE LEFT : " + str(0))
+
+else:
+    print("---------------------------------")
+    print("Better Luck Next Time!")
+    print("The Word was : " + mdata)
+    print("---------------------------------")
