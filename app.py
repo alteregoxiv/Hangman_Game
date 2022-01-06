a0d5c373fb4f384948446cd46f9e63fdc6b06746
alteregoxiv <prasad.biswasxiv@gmail.com>
2022-01-07 00:10:25 +0530

    basic design done word fetching from backend game initiation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask , render_template , request
+from flask import Flask , render_template , request , jsonify
+import requests , json
 
 app = Flask(__name__)
 
@@ -7,7 +8,13 @@ def index():
     if request.method=='GET':
         return render_template('index.html')
     else:
-        pass
+        httpresponse = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+        textdata = httpresponse.text
+        jsondata = json.loads(textdata)
+        mdata = jsondata[0]
+
+        return jsonify(dict(word=mdata))
+
 
 if __name__=="__main__":
     app.run(debug = True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,41 @@
 function startgame(){
 
+    var strt = document.getElementsByClassName("start")[0];
+    var game = document.getElementsByClassName("game")[0];
+
+    fetch("/" , {method: 'POST'})
+    .then(res=>res.json())
+    .then(
+        function(data){
+            strt.style.display = "none";
+            game.style.display = "block";
+            initialgame(data.word);
+        }
+    )
+    
+    
+    
+}
+
+
+var l , mword , dword , sword;
+function initialgame(word){
+
+    l = word.length;
+    mword = word;
+    dword = word.split("");
+    sword = Array(l).fill("_");
+    stat = document.getElementsByClassName("status")[0];
+    stat.innerHTML = sword.join(" ");
+
+}
+
+function playgame(){
+
+    guess = document.getElementsByClassName("guess")[0].value;
+    stat = document.getElementsByClassName("status")[0];
+    lives = Number(document.getElementsByClassName("lives")[0].innerHTML);
+
     
 
 }
