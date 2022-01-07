2ddcc858f18388569ff4403318e58b87fa576106
alteregoxiv <prasad.biswasxiv@gmail.com>
2022-01-07 12:35:00 +0530

    wrapped functions in js to prevent global access to variables

--- static/js/index.js
+++ static/js/index.js
@@ -5,96 +5,105 @@ guess.addEventListener("keyup", function(event) {
     }
 });
 
-function startgame(){
 
-    var strt = document.getElementsByClassName("start")[0];
-    var game = document.getElementsByClassName("game")[0];
-
-    fetch("/" , {method: 'POST'})
-    .then(res=>res.json())
-    .then(
-        function(data){
-            strt.style.display = "none";
-            game.style.display = "block";
-            initialgame(data.word);
-        }
-    )
+allfuncs = (function(){
+    startgame = function(){
     
+        var strt = document.getElementsByClassName("start")[0];
+        var game = document.getElementsByClassName("game")[0];
     
-    
-}
-
-
-var l , mword , dword , sword , already_guessed;
-function initialgame(word){
-
-    l = word.length;
-    mword = word;
-    dword = word.split("");
-    sword = Array(l).fill("_");
-    already_guessed = [];
-    stat = document.getElementsByClassName("status")[0];
-    stat.innerHTML = sword.join(" ");
-
-}
-
-function playgame(){
-
-    guess = document.getElementsByClassName("guess")[0].value;
-    stat = document.getElementsByClassName("status")[0];
-    lives = Number(document.getElementsByClassName("lives")[0].innerHTML);
-
-    if(guess.length>1 || !isNaN(guess)){
-
-        alert("Please enter a letter.");
-        return;
-
+        fetch("/" , {method: 'POST'})
+        .then(res=>res.json())
+        .then(
+            function(data){
+                strt.style.display = "none";
+                game.style.display = "block";
+                initialgame(data.word);
+            }
+        )
+        
+        
+        
     }
-
-    if(dword.includes(guess)){
-
-        already_guessed[already_guessed.length] = guess;
-        countguess = mword.split(guess).length - 1;
-        while(countguess>0){
-            index = dword.indexOf(guess);
-            dword[index] = 0;
-            sword[index] = guess;
-            countguess--;
-        }
+    
+    
+    var l , mword , dword , sword , already_guessed;
+    initialgame = function(word){
+    
+        l = word.length;
+        mword = word;
+        dword = word.split("");
+        sword = Array(l).fill("_");
+        already_guessed = [];
+        stat = document.getElementsByClassName("status")[0];
         stat.innerHTML = sword.join(" ");
-
-        if(sword.join("")==mword){
-
-            document.getElementsByClassName("guess")[0].style.display = "none";
-            document.getElementsByClassName("guessbutton")[0].style.display = "none";
-            document.getElementsByClassName("victory")[0].innerHTML = "Congratulations! Your guesses were on point";
-
-        }
-
+    
     }
-    else{
-
-        if(already_guessed.includes(guess)){
-
-            alert("You've already guessed " + guess);
+    
+    playgame = function(){
+    
+        guess = document.getElementsByClassName("guess")[0].value;
+        stat = document.getElementsByClassName("status")[0];
+        lives = Number(document.getElementsByClassName("lives")[0].innerHTML);
+    
+        if(guess.length>1 || !isNaN(guess)){
+    
+            alert("Please enter a letter.");
             return;
-
+    
         }
-
-        else{
-            
-            if(lives==1){
-
+    
+        if(dword.includes(guess)){
+    
+            already_guessed[already_guessed.length] = guess;
+            countguess = mword.split(guess).length - 1;
+            while(countguess>0){
+                index = dword.indexOf(guess);
+                dword[index] = 0;
+                sword[index] = guess;
+                countguess--;
+            }
+            stat.innerHTML = sword.join(" ");
+    
+            if(sword.join("")==mword){
+    
                 document.getElementsByClassName("guess")[0].style.display = "none";
                 document.getElementsByClassName("guessbutton")[0].style.display = "none";
-                document.getElementsByClassName("over")[0].innerHTML = "The word was : " + mword + "<br>Better luck next time!";
-
+                document.getElementsByClassName("victory")[0].innerHTML = "Congratulations! Your guesses were on point";
+    
             }
-            document.getElementsByClassName("lives")[0].innerHTML = lives - 1;
-            already_guessed[already_guessed.length] = guess;
-
+    
         }
-
+        else{
+    
+            if(already_guessed.includes(guess)){
+    
+                alert("You've already guessed " + guess);
+                return;
+    
+            }
+    
+            else{
+                
+                if(lives==1){
+    
+                    document.getElementsByClassName("guess")[0].style.display = "none";
+                    document.getElementsByClassName("guessbutton")[0].style.display = "none";
+                    document.getElementsByClassName("over")[0].innerHTML = "The word was : " + mword + "<br>Better luck next time!";
+    
+                }
+                document.getElementsByClassName("lives")[0].innerHTML = lives - 1;
+                already_guessed[already_guessed.length] = guess;
+    
+            }
+    
+        }
+    
     }
+    
+    return [initialgame , playgame , startgame];
+})()
 
-}
+initialgame = allfuncs[0];
+playgame = allfuncs[1];
+startgame = allfuncs[2];
